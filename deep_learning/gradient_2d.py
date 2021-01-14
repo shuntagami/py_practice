2bbd9cf1028c6e5147fee66369711e932d74e348
shuntagami <shuntagami23@gmail.com>
2021-01-14 12:17:16 +0900

    勾配法による最小値の算出

---
+++ deep_learning/gradient_2d.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+# cf.http://d.hatena.ne.jp/white_wheels/20100327/p3
+import numpy as np
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h)の計算
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)の計算
+        x[idx] = tmp_val -h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr*grad
+
+    return x
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+ans = gradient_descent(function_2, init_x = init_x, lr=0.1, step_num=100)
+print(ans)
