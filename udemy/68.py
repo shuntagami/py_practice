ac3a9bbe56b6cc416224e0257f927fd437be7230
shuntagami <shuntagami23@gmail.com>
2021-01-21 21:42:15 +0900

    lesson86

--- udemy/68.py
+++
@@ -1,7 +0,0 @@
-# import lesson_package.utils
-from lesson_package import utils
-from lesson_package.talk import human
-
-
-print(human.sing())
-print(human.cry())
---
+++ udemy/lesson.py
@@ -0,0 +1,11 @@
+# import lesson_package.utils
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+from lesson_package.talk import *
+
+print(animal.sing())
+print(animal.cry())
+
+
+print(human.sing())
+print(human.cry())
